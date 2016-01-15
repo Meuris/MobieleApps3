d6525aa13c93774cf90e3b3066fdfc89da27db30
Michiel <michielmeuris@hotmail.com>
2016-01-15 11:36:49 +0100

    Button tekst in het nederlands

--- app/src/main/java/com/example/projectsmobieleapps/restaurantapp/MainActivity.java
+++ app/src/main/java/com/example/projectsmobieleapps/restaurantapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
